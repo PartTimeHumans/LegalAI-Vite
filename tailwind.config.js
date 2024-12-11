cb8d41e98aed77e4d9bbd655eb48fe41be096349
Ayush <ayush421301@gmail.com>
2024-12-11 20:42:30 +0530

    Offloaded Waste Assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFD700',
+        'primary-dark': '#E6C200',
+        secondary: '#FFF0B3',
+        background: '#FFFDF5',
+      },
+    },
   },
   plugins: [],
 }
